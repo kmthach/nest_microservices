5ab66c6db9e72cc7145701f6b874b3b2664a7d1e
Nguyen Kim Thach <kmthach.ai@gmail.com>
2023-04-19 17:03:23 +0700

    update project 19-04

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -7,6 +7,8 @@ import { UserHashPassword } from './entities/user-password.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from 'src/constants/jwt.constant';
 import { UserRole } from './entities/user-role.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AmqpModule } from 'nestjs-amqp';
 
 @Module({
   imports: [
@@ -24,7 +26,29 @@ import { UserRole } from './entities/user-role.entity';
       global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
-    })],
+    }),
+    ClientsModule.register([
+      {
+        name: 'USER_QUEUE_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://root:root@localhost:5000'],
+          queue: 'user_queue',
+          queueOptions: {
+            durable: true
+          }
+        }
+      }
+    ]),
+    AmqpModule.forRoot({
+      name: 'rabbitmq',
+      hostname: 'localhost',
+      port: 5000,
+      username: 'root',
+      password: 'root',
+    }),
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
